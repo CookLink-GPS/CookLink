545e95eb1ccb8025f8bdf71283cb5ad0b5f2ca45
rodrimartineez <rodrim10@ucm.es>
2025-03-24 12:38:17 +0100

    busqueda actualizada

--- public/js/searchIngredient.js
+++ public/js/searchIngredient.js
@@ -2,16 +2,22 @@
 const searchInput = document.getElementById("searchInput");
 const ingredientList = document.getElementById("ingredientList");
 const ingredients = Array.from(ingredientList.getElementsByClassName("ingredient-item"));
-const MIN_FILTER_LENGTH = 3;
-
+const MIN_FILTER_LENGTH = 2;
+const ZERO = 0;
 
 const removeAccents = text => text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
-searchInput.addEventListener("input", () => {
-	const filter = removeAccents(searchInput.value.toLowerCase());
-	if (filter.length < MIN_FILTER_LENGTH) ingredients.forEach(item => item.style.display = "none");
-	else ingredients.forEach(item => {
+const filterIngredients = (input, ingredientsList, filterFunc) => {
+	const filter = filterFunc(input);
+	if (filter.length === ZERO) ingredientsList.forEach(item => item.style.display = "");
+	else if (filter.length < MIN_FILTER_LENGTH) ingredientsList.forEach(item => item.style.display = "none");
+	else ingredientsList.forEach(item => {
 		const text = removeAccents(item.textContent.toLowerCase());
 		item.style.display = text.includes(filter) ? "" : "none";
 	});
+};
+
+searchInput.addEventListener("input", () => {
+	filterIngredients(searchInput.value, ingredients, removeAccents);
 });
+
