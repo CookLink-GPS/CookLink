beb5071c66aa70ffee9e72d180138566720884e3
luisjg04 <luisjg04@ucm.es>
2025-03-24 19:04:35 +0100

    cambios en la busqueda de ingrediente

--- public/js/searchIngredient.js
+++ public/js/searchIngredient.js
@@ -2,22 +2,32 @@
 const searchInput = document.getElementById("searchInput");
 const ingredientList = document.getElementById("ingredientList");
 const ingredients = Array.from(ingredientList.getElementsByClassName("ingredient-item"));
-const MIN_FILTER_LENGTH = 2;
+const MIN_FILTER_LENGTH = 0;
 const ZERO = 0;
+let changed = false;
+const ms = 100;
 
-const removeAccents = text => text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+const removeAccents = text => text.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+	.trim();
 
-const filterIngredients = (input, ingredientsList, filterFunc) => {
-	const filter = filterFunc(input);
-	if (filter.length === ZERO) ingredientsList.forEach(item => item.style.display = "");
-	else if (filter.length < MIN_FILTER_LENGTH) ingredientsList.forEach(item => item.style.display = "none");
-	else ingredientsList.forEach(item => {
+const addDelay = (func, delay) => {
+	if (!changed) {
+		changed = true;
+		setTimeout(() => changed = false, delay);
+		return func();
+	}
+};
+
+const filterList = (filter, list) => {
+	const normalizedFilter = removeAccents(filter.toLowerCase());
+
+	if (normalizedFilter.length === ZERO) list.forEach(item => item.style.display = "");
+	else if (normalizedFilter.length < MIN_FILTER_LENGTH) list.forEach(item => item.style.display = "none");
+	else list.forEach(item => {
 		const text = removeAccents(item.textContent.toLowerCase());
-		item.style.display = text.includes(filter) ? "" : "none";
+		item.style.display = text.startsWith(normalizedFilter) ? "" : "none";
 	});
 };
 
-searchInput.addEventListener("input", () => {
-	filterIngredients(searchInput.value, ingredients, removeAccents);
-});
+searchInput.addEventListener("input", () => addDelay(() => filterList(searchInput.value, ingredients), ms));
 
