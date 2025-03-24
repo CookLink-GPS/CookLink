65a544364cf889c66951bb026adf63568d4a987d
rodrimartineez <rodrim10@ucm.es>
2025-03-24 12:04:19 +0100

    cambio en busqueda

--- public/js/searchIngredient.js
+++ public/js/searchIngredient.js
@@ -2,14 +2,15 @@
 const searchInput = document.getElementById("searchInput");
 const ingredientList = document.getElementById("ingredientList");
 const ingredients = Array.from(ingredientList.getElementsByClassName("ingredient-item"));
+const MIN_FILTER_LENGTH = 3;
 
 
 const removeAccents = text => text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
 searchInput.addEventListener("input", () => {
 	const filter = removeAccents(searchInput.value.toLowerCase());
-
-	ingredients.forEach(item => {
+	if (filter.length < MIN_FILTER_LENGTH) ingredients.forEach(item => item.style.display = "none");
+	else ingredients.forEach(item => {
 		const text = removeAccents(item.textContent.toLowerCase());
 		item.style.display = text.includes(filter) ? "" : "none";
 	});
