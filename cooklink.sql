dc1dc865156c555b8b29d8278a9a6386613df1c0
Albertomog88 <almore03@ucm.es>
2025-03-26 00:50:32 +0100

    bbd correcta? Definitva?

--- cooklink.sql
+++ cooklink.sql
@@ -1,83 +1,37 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 26-03-2025 a las 00:46:24
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `cooklink`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `despensa`
---
-
-CREATE TABLE `despensa` (
-  `id_despensa` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `id_ingrediente` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL CHECK (`cantidad` > 0)
+CREATE TABLE usuarios (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  username varchar(50) NOT NULL UNIQUE CHECK (TRIM(username) != "" AND username NOT LIKE "% %"),
+  password varchar(255) NOT NULL CHECK (TRIM(password) != "" AND password NOT LIKE "% %")
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `despensa`
---
+CREATE TABLE ingredientes (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  nombre varchar(50) NOT NULL UNIQUE,
+  tipoUnidad varchar(50) NOT NULL    
+);
 
-INSERT INTO `despensa` (`id_despensa`, `id_usuario`, `id_ingrediente`, `cantidad`) VALUES
-(5, 1, 4, 5),
-(6, 1, 5, 3009),
-(7, 1, 6, 15),
-(8, 1, 7, 15);
+CREATE TABLE recetas (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  nombre varchar(255) NOT NULL UNIQUE,
+  descripcion TEXT
+);
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `despensa`
---
-ALTER TABLE `despensa`
-  ADD PRIMARY KEY (`id_despensa`),
-  ADD KEY `fk_usuario` (`id_usuario`),
-  ADD KEY `fk_ingrediente` (`id_ingrediente`);
+CREATE TABLE despensa (
+  id_usuario INT,
+  id_ingrediente INT,
+  caducidad varchar(10) NOT NULL, 
+  cantidad INT NOT NULL CHECK (cantidad > 0),
+  PRIMARY KEY (id_usuario, id_ingrediente),
+  CONSTRAINT fk_usuario_despensa FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE,
+  CONSTRAINT fk_ingrediente_despensa FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id) ON DELETE CASCADE
+);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `despensa`
---
-ALTER TABLE `despensa`
-  MODIFY `id_despensa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `despensa`
---
-ALTER TABLE `despensa`
-  ADD CONSTRAINT `fk_ingrediente` FOREIGN KEY (`id_ingrediente`) REFERENCES `ingredientes` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE table contiene (
+  id_receta INT,
+  id_ingrediente INT,
+  unidades INT CHECK (unidades > 0),
+  PRIMARY KEY (id_receta, id_ingrediente),
+  CONSTRAINT fk_receta_contiene FOREIGN KEY (id_receta) REFERENCES recetas(id) ON DELETE CASCADE,
+  CONSTRAINT fk_ingrediente_contiene FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id) ON DELETE CASCADE
+);
