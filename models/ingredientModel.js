be132314e2c859790d93ea4ebc0295b7695218e8
rodrimartineez <rodrim10@ucm.es>
2025-03-25 17:00:22 +0100

    correccion

--- models/ingredientModel.js
+++ models/ingredientModel.js
@@ -23,11 +23,29 @@ const Ingredient = {
 
 	},
 
+	/**
+	 * Description placeholder
+	 *
+	 * @async
+	 * @returns {Promise<Ingredient[]>}
+	 */
+	async getAllIngredients() {
+		try {
+			const sql = `SELECT * FROM ${nombreTabla}`;
+			const res = await db.query(sql);
+
+			return res.map(row => ({ ...row }));
+		}
+		catch (error) {
+			console.log(error);
+			throw Error("Error al obtener los ingredientes");
+		}
+	},
+
 	async getIngredient(id) {
 		try {
 			const sql = `SELECT * FROM ${nombreTabla} WHERE id = ?`;
 			const rows = await db.promise().query(sql, [ id ]);
-
 			let ingredient;
 			// eslint-disable-next-line no-magic-numbers
 			if (rows.length > 0) ingredient = rows[0];
