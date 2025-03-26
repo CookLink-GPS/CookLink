e517a750ea2fd71b1195fcdf0cb79c4dbe564761
cecilia0290 <160527906+cecilia0290@users.noreply.github.com>
2025-03-26 03:13:55 +0100

    Creado nueva feature de añadir ingredientes BD a la despensa

--- config/queries.js
+++ config/queries.js
@@ -10,7 +10,9 @@ module.exports.recipeQueries = { getAllRecipes: "SELECT id, nombre, descripcion 
 module.exports.pantryQueries = {
 	getPantryFromUser: "SELECT id_ingrediente, caducidad, cantidad FROM despensa WHERE id_usuario = ?",
 	getIngredientsDetails: "SELECT * FROM despensa d JOIN ingredientes i ON (d.id_ingrediente = i.id) WHERE id_usuario = ? ORDER BY i.nombre",
-	deleteIngredient: "DELETE FROM despensa WHERE id_usuario = ? AND id_ingrediente = ?"
+	deleteIngredient: "DELETE FROM despensa WHERE id_usuario = ? AND id_ingrediente = ?",
+	addIngrediente: "INSERT INTO despensa (id_usuario, id_ingrediente, caducidad, cantidad) VALUES (?, ?, ?, ?)",
+	getPantryFromUserWithNameIngredient: "SELECT d.id_ingrediente, i.nombre, d.caducidad, d.cantidad FROM despensa d JOIN ingredientes i ON d.id_ingrediente = i.id WHERE d.id_usuario = ? ORDER BY i.nombre"
 };
 
 module.exports.containsQueries = { getFromRecipe: "SELECT id_ingrediente, unidades FROM contiene WHERE id_receta = ?" };
