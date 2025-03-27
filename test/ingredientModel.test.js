9d17f1a2c49107ee4041730a662ee63edd865a64
rodrimartineez <rodrim10@ucm.es>
2025-03-27 21:38:33 +0100

    tests casi hechos

---
+++ test/ingredientModel.test.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-magic-numbers */
+/* eslint-disable no-undef */
+
+const assert = require("assert");
+const { deleteIngredients } = require("./testUtils");
+const Ingredient = require("../models/ingredientModel");
+
+describe("Modelo Ingrediente", () => {
+	before(deleteIngredients);
+	beforeEach(deleteIngredients);
+
+	describe("Añadir ingrediente", () => {
+		it("Debe crear correctamente un ingrediente nuevo con datos válidos", async () => {
+			const nombre = "Tomate";
+			const tipoUnidad = "kg";
+
+			let good = true;
+			let id;
+			try {
+				id = await Ingredient.create(nombre, tipoUnidad);
+			}
+			catch (err) {
+				good = false;
+			}
+
+			assert.ok(good);
+			assert.ok(id > 0);
+		});
+
+		it("No debe crear un ingrediente sin nombre", async () => {
+			let good = false;
+			try {
+				await Ingredient.create("", "kg");
+			}
+			catch (err) {
+				good = true;
+			}
+			assert.ok(good);
+		});
+
+		it("No debe crear un ingrediente sin unidad de medida", async () => {
+			let good = false;
+			try {
+				await Ingredient.create("Tomate", "");
+			}
+			catch (err) {
+				good = true;
+			}
+			assert.ok(good);
+		});
+	});
+});
---
+++ test/ingredientService.test.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-undef */
+
+const assert = require("assert");
+const { deleteIngredients, deletePantryItems } = require("./testUtils");
+const IngredientService = require("../services/ingredientService");
+
+describe("Servicio Ingredient", () => {
+	before(async () => {
+		await deleteIngredients();
+		await deletePantryItems();
+	});
+	beforeEach(async () => {
+		await deleteIngredients();
+		await deletePantryItems();
+	});
+
+	describe("processIngredient", () => {
+		it("Debe sumar la cantidad si el ingrediente ya existe y las unidades coinciden", async () => {
+			const data = { nombre: "Tomate", tipoUnidad: "kg", cantidad: 2, userId: 1 };
+
+			const res1 = await IngredientService.processIngredient(data);
+			const res2 = await IngredientService.processIngredient(data);
+
+			assert.strictEqual(res1.action, "added");
+			assert.strictEqual(res2.action, "updated");
+		});
+
+		it("Debe lanzar un error si el ingrediente ya existe pero las unidades no coinciden", async () => {
+			const data1 = { nombre: "Tomate", tipoUnidad: "kg", cantidad: 2, userId: 1 };
+			const data2 = { nombre: "Tomate", tipoUnidad: "g", cantidad: 3, userId: 1 };
+
+			let good = false;
+			try {
+				await IngredientService.processIngredient(data1);
+				await IngredientService.processIngredient(data2);
+			}
+			catch (err) {
+				good = true;
+			}
+			assert.ok(good);
+		});
+
+		it("Debe lanzar un error si la cantidad es igual a cero", async () => {
+			const data = { nombre: "Tomate", tipoUnidad: "kg", cantidad: 0, userId: 1 };
+
+			let good = false;
+			try {
+				await IngredientService.processIngredient(data);
+			}
+			catch (err) {
+				good = true;
+			}
+			assert.ok(good);
+		});
+
+		it("Debe lanzar un error si la cantidad es menor que cero", async () => {
+			const data = { nombre: "Tomate", tipoUnidad: "kg", cantidad: -1, userId: 1 };
+
+			let good = false;
+			try {
+				await IngredientService.processIngredient(data);
+			}
+			catch (err) {
+				good = true;
+			}
+			assert.ok(good);
+		});
+	});
+});
