dc4559c95277a1dfd23179b6384060c3d1d040ac
rodrimartineez <rodrim10@ucm.es>
2025-03-27 21:38:33 +0100

    tests casi hechos

--- test/ingredientRoutes.test.js
+++ test/ingredientRoutes.test.js
@@ -50,6 +50,164 @@ describe("Rutas ingrediente", () => {
 
 			assert.strictEqual(ingredientes.length, harinas.length);
 		});
+	});
 
+
+	describe("Agregar ingrediente", () => {
+		const route = `${baseRoute}/add`;
+
+		// CL_003_01: Añadir un nuevo ingrediente correctamente
+		it("Debe agregar un nuevo ingrediente correctamente a la despena cuando se introduce un nombre, unidad y cantidad válidos", async () => {
+			const ingrediente = {
+				nombre: "Tomate",
+				tipoUnidad: "kg",
+				cantidad: 2,
+				userId: 1
+			};
+
+			const res = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			assert.equal(res.status, ok);
+		});
+
+		// CL_003_02: Error si falta algún campo
+		it("No debe agregar el ingrediente si falta algún campo", async () => {
+			const ingrediente = {
+				nombre: "",
+				tipoUnidad: "kg",
+				cantidad: 2,
+				userId: 1
+			};
+
+			const res = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			assert.equal(res.status, badRequest);
+		});
+
+		// CL_003_03: Error si la cantidad es menor que cero
+		it("No debe agregar el ingrediente si la cantidad es menor que cero", async () => {
+			const ingrediente = {
+				nombre: "Tomate",
+				tipoUnidad: "kg",
+				cantidad: -1,
+				userId: 1
+			};
+
+			const res = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			assert.equal(res.status, badRequest);
+		});
+
+		// CL_003_03: Error si la cantidad es igual a cero
+		it("No debe agregar el ingrediente si la cantidad es igual a cero", async () => {
+			const ingrediente = {
+				nombre: "Tomate",
+				tipoUnidad: "kg",
+				cantidad: 0,
+				userId: 1
+			};
+
+			const res = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			assert.equal(res.status, badRequest);
+		});
+
+		// CL_003_04: Error si no se selecciona ninguna unidad de medida
+		it("No debe agregar el ingrediente si no se selecciona unidad de medida", async () => {
+			const ingrediente = {
+				nombre: "Tomate",
+				tipoUnidad: "",
+				cantidad: 2,
+				userId: 1
+			};
+
+			const res = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			assert.equal(res.status, badRequest);
+		});
+
+		// CL_003_05: Si ya existe y las unidades coinciden, se suma la cantidad
+		it("Si el ingrediente ya existe en la despensa y las unidades coinciden, se debe sumar la cantidad", async () => {
+			const ingrediente = {
+				nombre: "Tomate",
+				tipoUnidad: "kg",
+				cantidad: 2,
+				userId: 1
+			};
+
+			const res1 = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			const ingrediente2 = {
+				nombre: "Tomate",
+				tipoUnidad: "kg",
+				cantidad: 3,
+				userId: 1
+			};
+
+			const res2 = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente2),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			assert.equal(res1.status, ok);
+			assert.equal(res2.status, ok);
+		});
+
+		// CL_003_06: Error si ya existe pero las unidades no coinciden
+		it("Si el ingrediente ya existe en la despensa pero las unidades no coinciden, se debe mostrar error", async () => {
+			const ingrediente = {
+				nombre: "Tomate",
+				tipoUnidad: "kg",
+				cantidad: 2,
+				userId: 1
+			};
+
+			const res1 = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			const ingrediente2 = {
+				nombre: "Tomate",
+				tipoUnidad: "g",
+				cantidad: 3,
+				userId: 1
+			};
+
+			const res2 = await fetch(route, {
+				method: "POST",
+				body: JSON.stringify(ingrediente2),
+				headers: { "Content-Type": "application/json" }
+			});
+
+			assert.equal(res1.status, ok);
+			assert.equal(res2.status, conflict);
+		});
 	});
 });
