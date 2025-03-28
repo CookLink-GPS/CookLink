9b0e24fb43aa75a6748cac1d5669a97648273591
luisjg04 <148762228+luisjg04@users.noreply.github.com>
2025-03-28 11:20:53 +0100

    Version definitiva bbdd

--- cooklink.sql
+++ cooklink.sql
@@ -1,53 +1,44 @@
+DROP TABLE contiene;
+DROP TABLE despensa;
 DROP TABLE usuarios;
 DROP TABLE ingredientes;
 DROP TABLE recetas;
-DROP TABLE despensa;
-DROP TABLE contiene;
 
 CREATE TABLE usuarios (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  username varchar(50) NOT NULL UNIQUE CHECK (TRIM(username) != "" AND username NOT LIKE "% %"),
-  password varchar(255) NOT NULL CHECK (TRIM(password) != "" AND password NOT LIKE "% %")
+  id int PRIMARY KEY AUTO_INCREMENT,
+  username varchar(50) UNIQUE NOT NULL CHECK (trim(username) <> '' and username not like '% %'),
+  password varchar(255) NOT NULL CHECK (trim(password) <> '' and password not like '% %')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE ingredientes (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  nombre varchar(50) NOT NULL UNIQUE,
-  tipoUnidad varchar(50) NOT NULL    
-);
+  id int PRIMARY KEY AUTO_INCREMENT,
+  nombre varchar(50) UNIQUE NOT NULL,
+  tipoUnidad varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE recetas (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  nombre varchar(255) NOT NULL UNIQUE,
-  descripcion TEXT
-);
+  id int PRIMARY KEY AUTO_INCREMENT,
+  nombre varchar(255) UNIQUE NOT NULL,
+  descripcion text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE contiene (
+  id_receta int NOT NULL,
+  id_ingrediente int NOT NULL,
+  unidades int DEFAULT NULL CHECK (unidades > 0),
+  PRIMARY KEY (id_receta, id_ingrediente),
+  CONSTRAINT fk_ingrediente_contiene FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id) ON DELETE CASCADE,
+  CONSTRAINT fk_receta_contiene FOREIGN KEY (id_receta) REFERENCES recetas(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE despensa (
-<<<<<<< HEAD
-id_despensa int AUTO_INCREMENT PRIMARY KEY,
-=======
-  id_despensa int AUTO_INCREMENT PRIMARY KEY,
->>>>>>> 142c896 (MAL)
-  id_usuario INT NOT NULL,
-  id_ingrediente INT NOT NULL,
-  cantidad INT NOT NULL CHECK (cantidad > 0),
-  CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE,
-  CONSTRAINT fk_ingrediente FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id) ON DELETE CASCADE,
-<<<<<<< HEAD
-=======
-  CONSTRAINT fk_usuario_ingrediente_caducidad UNIQUE (id_usuario, id_ingrediente, caducidad)
->>>>>>> 142c896 (MAL)
-);
-
-CREATE table contiene (
-  id_receta INT,
-  id_ingrediente INT,
-  unidades INT CHECK (unidades > 0),
-  PRIMARY KEY (id_receta, id_ingrediente),
-  CONSTRAINT fk_receta FOREIGN KEY (id_receta) REFERENCES recetas(id) ON DELETE CASCADE,
-  CONSTRAINT fk_ingrediente_contiene FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id) ON DELETE CASCADE
-<<<<<<< HEAD
+  id_despensa int PRIMARY KEY AUTO_INCREMENT,
+  id_usuario int NOT NULL,
+  id_ingrediente int NOT NULL,
+  cantidad float NOT NULL,
+  CONSTRAINT fk_ingrediente_despensa FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id) ON DELETE CASCADE,
+  CONSTRAINT fk_usuario_despensa FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 );
 =======
