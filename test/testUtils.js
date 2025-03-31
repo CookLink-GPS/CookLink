2e84e98cddcadbe681ed37295a2faf4b98e996fb
Paula211 <paulalsolla@gmail.com>
2025-03-31 21:58:33 +0200

    intento de mejora de rendimiento de tests

--- test/testUtils.js
+++ test/testUtils.js
@@ -32,34 +32,75 @@ const deleteIngredients = async () => {
  * @returns {void}
  */
 const insertIngredients = async ingredients => {
-	for (const ingredient of ingredients) await db.query("INSERT INTO ingredientes (nombre, tipoUnidad) VALUES (?, ?)", ingredient);
+	const insertPromises = ingredients.map(ingredient =>
+		db.query("INSERT INTO ingredientes (nombre, tipoUnidad) VALUES (?, ?)", ingredient));
+	await Promise.all(insertPromises); // Ejecuta todas las inserciones en paralelo
 	return db.query("SELECT id, nombre, tipoUnidad FROM ingredientes");
 };
 
+/**
+ * Elimina todos los registros de la despensa (tabla contiene y despensa)
+ *
+ * @returns {Promise<void>}
+ */
 const deletePantry = async () => {
 	await db.query("DELETE FROM contiene"); // Si existe
 	await db.query("DELETE FROM despensa");
 };
 
+/**
+ * Inserta varios registros en la despensa
+ *
+ * @param {Array} pantrys - Arreglo con los datos de la despensa
+ * @returns {Promise<void>}
+ */
 const insertPantry = async pantrys => {
-	for (const pantry of pantrys) await db.query("INSERT INTO despensa (id_usuario, id_ingrediente, cantidad) VALUES (?, ?, ?)", pantry);
+	const insertPromises = pantrys.map(pantry =>
+		db.query("INSERT INTO despensa (id_usuario, id_ingrediente, cantidad) VALUES (?, ?, ?)", pantry));
+	await Promise.all(insertPromises); // Ejecuta todas las inserciones en paralelo
 };
 
+/**
+ * Elimina todas las recetas de la base de datos.
+ *
+ * @returns {Promise<void>}
+ */
 const deleteRecipes = async () => {
 	await db.query("DELETE FROM recetas");
 };
 
+/**
+ * Inserta varias recetas
+ *
+ * @param {Array} recetas - Arreglo con los datos de las recetas
+ * @returns {Promise<Array>} - Devuelve una consulta con los id y nombre de las recetas
+ */
 const insertRecetas = async recetas => {
-	for (const receta of recetas) await db.query("INSERT INTO recetas (nombre, descripcion) VALUES (?, ?)", receta);
+	const insertPromises = recetas.map(receta =>
+		db.query("INSERT INTO recetas (nombre, descripcion) VALUES (?, ?)", receta));
+	await Promise.all(insertPromises); // Ejecuta todas las inserciones en paralelo
 	return db.query("SELECT id, nombre FROM recetas");
 };
 
+/**
+ * Elimina todos los registros de la tabla contiene.
+ *
+ * @returns {Promise<void>}
+ */
 const deleteContains = async () => {
 	await db.query("DELETE FROM contiene");
 };
 
+/**
+ * Inserta varios registros en la tabla contiene (relacion entre recetas e ingredientes)
+ *
+ * @param {Array} contains - Arreglo con los datos de las relaciones
+ * @returns {Promise<void>}
+ */
 const insertContains = async contains => {
-	for (const contain of contains) await db.query("INSERT INTO contiene (id_receta, id_ingrediente, unidades) VALUES (?, ?, ?)", contain);
+	const insertPromises = contains.map(contain =>
+		db.query("INSERT INTO contiene (id_receta, id_ingrediente, unidades) VALUES (?, ?, ?)", contain));
+	await Promise.all(insertPromises); // Ejecuta todas las inserciones en paralelo
 };
 
-module.exports = { deleteUsers, deleteIngredients, insertIngredients, deletePantry, insertPantry, deleteRecipes, insertRecetas, deleteContains, insertContains };
+module.exports = { deleteUsers,	deleteIngredients,	insertIngredients,	deletePantry,	insertPantry,	deleteRecipes,	insertRecetas,	deleteContains,	insertContains };
