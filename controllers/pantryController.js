420cef14b1a2a26c5200e7adfa46eeb2e71a9971
luisjg04 <luisjg04@ucm.es>
2025-03-31 19:52:33 +0200

    cambiadas algunas cosas

--- controllers/pantryController.js
+++ controllers/pantryController.js
@@ -4,12 +4,12 @@ const AppError = require("../utils/AppError");
 const { ok } = require("../config/httpcodes");
 const { renderView } = require("../middlewares/viewHelper");
 
-const pantryController = {
+const PantryController = {
 	/**
      * Displays the user's pantry.
      *
-     * @param {Object} req - HTTP request object.
-     * @param {Object} res - HTTP response object.
+     * @param {Request} req - HTTP request object.
+     * @param {Response} res - HTTP response object.
      */
 	async showPantry(req, res) {
 		try {
@@ -26,11 +26,10 @@ const pantryController = {
 	/**
      * Deletes an ingredient from the user's pantry.
      *
-     * @param {Object} req - HTTP request object.
-     * @param {Object} res - HTTP response object.
+     * @param {Request} req - HTTP request object.
+     * @param {Response} res - HTTP response object.
      */
 	async deleteIngredient(req, res) {
-		const ERROR = 500;
 		try {
 			const userId = req.session.user.id;
 			const idDespensa = req.params.id_despensa;
@@ -46,35 +45,34 @@ const pantryController = {
 			console.error(error.message);
 			res.status(error.status || ERROR).render("error", { message: error.message || "Error deleting ingredient" });
 		}
+	},
+	/**
+	 * Devuelve una lista con los ingredientes a buscar
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 */
+	async searchIngredients(req, res) {
+		try {
+			const ingredientes = await PantryService.searchIngredients(req.params.filter || "", req.session.user.id);
+
+			res.json({ ingredientes });
+		}
+		catch (err) {
+			console.error(err.message);
+			res.json({ mensajeError: "Error al filtrar los ingredientes" });
+		}
+	},
+	/**
+	 * Renderiza una vista con todos los ingredientes de la despensa del usuario
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 */
+	async getDespensa(req, res) {
+		const ingredients = await PantryService.getIngredientsDetails(req.session.user.id);
+		renderView(res, "despensa", ok, { ingredients });
 	}
 };
 
-/**
- * Renderiza una vista con todos los ingredientes de la despensa del usuario
- *
- * @param {Request} req
- * @param {Response} res
- */
-exports.getDespensa = async (req, res) => {
-	const ingredients = await PantryService.getIngredientsDetails(req.session.user.id);
-	renderView(res, "despensa", ok, { ingredients });
-};
-
-module.exports = pantryController;
-/**
- * Devuelve una lista con los ingredientes a buscar
- *
- * @param {Request} req
- * @param {Response} res
- */
-exports.searchIngredients = async (req, res) => {
-	try {
-		const ingredientes = await pantryService.searchIngredients(req.params.filter || "", req.session.user.id);
-
-		res.json({ ingredientes });
-	}
-	catch (err) {
-		console.error(err.message);
-		res.json({ mensajeError: "Error al filtrar los ingredientes" });
-	}
-};
+module.exports = PantryController;
