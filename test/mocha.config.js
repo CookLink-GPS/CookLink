5f22057d915e2527e123c83dff8b9aa5ed30b19a
Paula211 <paulalsolla@gmail.com>
2025-03-31 22:22:40 +0200

    .

--- test/mocha.config.js
+++ test/mocha.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	timeout: 8000, // Establece el timeout global a 5 segundos
+	timeout: 10000, // Establece el timeout global a 5 segundos
 	reporter: "spec" // Establece el reporter, por ejemplo, 'spec' para salida detallada
 };
