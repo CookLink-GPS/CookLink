5b8773232cab965ba8be1e28035bf6863925ab88
Albertomog88 <almore03@ucm.es>
2025-04-01 00:10:57 +0200

    pequeños cambios

--- test/sessionModel.test.js
+++ test/sessionModel.test.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-undef */
 
 const assert = require("assert");
+// Const bcrypt = require("bcrypt");
 const { deleteUsers, createTestUsers } = require("./testUtils");
 const User = require("../models/userModel");
 
 
 describe("Modelo usuario", () => {
-	before(deleteUsers); // Antes de todos los test
-	before(createTestUsers);
-	// BeforeEach(deleteUsers); // Despues de cada test
+	before(deleteUsers); // Antes de todos los test borramos usuarios
+	before(createTestUsers); // Creamos usuarios de prueba
+	beforeEach(deleteUsers); // Despues de cada test borramos usuarios
 
 	describe("Registro", () => {
 
 
-		it("Inicio de sesión con credenciales válidas", async () => {
+		it("✅ Inicio de sesión con credenciales válidas", async () => {
 			const usuario = {
 				username: "user1",
 				password: "12345678"
@@ -22,7 +23,10 @@ describe("Modelo usuario", () => {
 			let good = true;
 
 			try {
-				await User.inicio(usuario.username, usuario.password);
+				const result = await User.inicio(usuario);
+				assert.ok(result); // El usuario debería existir
+				assert.strictEqual(result.username, usuario.username); // Verificamos que el nombre de usuario sea correcto
+				assert.strictEqual(result.password, usuario.password); // Verificamos que la contraseña sea correcta
 			}
 			catch (err) {
 				good = false;
