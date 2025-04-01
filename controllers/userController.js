e46caa1bab1f845bf19d24f3083675588f324fa3
luisjg04 <luisjg04@ucm.es>
2025-04-01 23:40:55 +0200

    solucionado copia de logins

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,37 +22,12 @@ exports.getAllUsers = async (req, res, next) => {
 };
 
 exports.login = async (req, res) => {
-
-
-	const { username, password } = req.body;
-	try {
-		const user = await UserService.login( { username, password });
-
-		// Comparar la contraseña hashada en la base de datos
-		const valido = await bcrypt.compare(password, user.password);
-		if (!valido) return res.status(unauthorized).json({ error: "Credenciales inválidas" });
-
-		req.session.userId = user.id; // Guarda el ID del usuario en la sesión
-		req.session.username = user.username;
-
-		console.log("Sesión guardada:", req.session); // Debugging
-		renderView(res, "inicio", ok, { usuario: req.session.username });
-	}
-	catch (error) {
-		console.error(error);
-		renderView(res, "login", badRequest, { mensajeError: "Las Credenciales de acceso son incorrectas" });
-	}
-};
-
-exports.login = async (req, res) => {
 	const { username, password } = req.body;
 
 	try {
 		const user = await UserService.iniciarSesion({ username, password });
 
 		req.session.user = { ...user };
-
-		// Console.log("Sesión guardada:", req.session); // Debugging
 		renderView(res, "inicio", ok, { usuario: req.session.username });
 	}
 	catch (error) {
