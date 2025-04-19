fa346f989791a8de6dacfe2498c2a5a67926c96d
Paula211 <paulalsolla@gmail.com>
2025-04-19 22:23:26 +0200

    vista creada

---
+++ public/js/recipeScript.js
@@ -0,0 +1,9 @@
+// Mostrar el modal de éxito
+const exitoModal = document.getElementById("exitoModal");
+if (exitoModal) exitoModal.style.display = "block";
+
+
+// Mostrar el modal de error
+const errorModal = document.getElementById("errorModal");
+if (errorModal) errorModal.style.display = "block";
+
