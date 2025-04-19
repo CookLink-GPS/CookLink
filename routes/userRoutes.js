6cb353bef2fcc6e0f515e1a0b3a666867d5faea4
Paula211 <paulalsolla@gmail.com>
2025-04-19 19:45:54 +0200

    mensajes registro hecho

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,10 +13,24 @@ router.get(userRoutes.register, userController.toRegister);
 router.delete(userRoutes.delete, userController.deleteUser);
 router.post(
 	userRoutes.register,
-	check("password", "La contraseña debe contener al menos una letra minúscula, una letra mayúscula, un número y un carácter especial")
-		.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&'*+-/=.?^_{|}@(),:;<>@[])/),
-	check("password", "La longitud minima de la contraseña debe ser 8").isLength({ min: 8 }),
-	check("password", "La longitud máxima de la contraseña es de 50 carácteres").isLength({ max: 50 }),
+	check("password")
+		.custom(value => {
+			const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&'*+-/=.?^_{|}@(),:;<>@[])/;
+			if (!regex.test(value)) throw new AppError(
+				"La contraseña debe contener al menos una letra minúscula, una letra mayúscula, un número y un carácter especial",
+				badRequest
+			);
+			else if (regex.test(value) && value.length < 8) throw new AppError(
+				"La longitud mínima de la contraseña debe ser 8",
+				badRequest
+			);
+			else if (regex.test(value) && value.length > 50) throw new AppError(
+				"La longitud máxima de la contraseña es de 50 caracteres",
+				badRequest
+			);
+
+			return true;
+		}),
 	check("confirm_password")
 		.custom((value, { req }) => {
 			if (value !== req.body.password) throw new AppError("Las contraseñas no son iguales", badRequest);
