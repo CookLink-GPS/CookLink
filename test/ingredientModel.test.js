86b5435d494f28fd4201da8619c74fe02549278b
rodrimartineez <rodrim10@ucm.es>
2025-04-19 18:50:38 +0200

    test unitarios a medias

--- test/ingredientModel.test.js
+++ test/ingredientModel.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 /* eslint-disable no-undef */
 const assert = require("node:assert");
 const { deleteIngredients, insertIngredients } = require("./testUtils");
@@ -23,6 +24,34 @@ describe("Modelo ingrediente", () => {
 			ingredientes.forEach(ingrediente => {
 				assert.ok(res.find(({ nombre, tipoUnidad }) => ingrediente[0] === nombre && ingrediente[1] === tipoUnidad));
 			});
+		});
+	});
+
+	describe("Añadir ingrediente de la base de datos a la despensa", () => {
+		const ingredientes = [
+			[ "harina", "gramos" ],
+			[ "arroz", "gramos" ],
+			[ "leche", "litros" ]
+		];
+
+		it("Debe añadir el ingrediente seleccionado de la base de datos a la despensa", async () => {
+			const id = await insertIngredients(ingredientes);
+
+			const res = await Ingredient.getIngredient(id[0]);
+
+			assert.strictEqual(res.nombre, ingredientes[0][0]);
+			assert.strictEqual(res.tipoUnidad, ingredientes[0][1]);
+		});
+
+		it("Debe devolver un error si no selecciona ningun ingrediente de la base de datos", async () => {
+			const id = await insertIngredients(ingredientes);
+
+			try {
+				await Ingredient.getIngredientById(id[0] + 1);
+			}
+			catch (error) {
+				assert.strictEqual(error.message, `Error obteniendo el ingrediente ${id[0] + 1}`);
+			}
 		});
 	});
 });
