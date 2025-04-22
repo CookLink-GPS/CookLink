c86de2cc67cb7b46b6c3d3bfb70a860583e5e54b
iXian <147181020+isaXCC@users.noreply.github.com>
2025-04-22 14:36:46 +0200

    tests de servicio de HU 010

--- test/recipe/recipeService.test.js
+++ test/recipe/recipeService.test.js
@@ -2,17 +2,29 @@
 
 const assert = require("assert");
 const Recipe = require("../../services/recipeService");
-const { deleteIngredients, insertIngredients, insertPantry, deletePantry, deleteUsers, insertRecetas, deleteRecipes, insertContains, deleteContains } = require("../testUtils");
+const { deleteIngredients, insertIngredients, insertPantry, deletePantry, deleteUsers, insertRecetas, deleteRecipes, insertContains, deleteContains, insertListaCompra, deleteListaCompra, } = require("../testUtils");
 const User = require("../../models/userModel");
 const db = require("../../config/database");
 const CERO = 0;
 const QUINIENTOS = 500;
+const SETECIENTOS = 700;
+const TRESCIENTOS = 300;
+const DOSCIENTOS = 200;
 
 
 describe("Servicio de recetas", () => {
 	let user, user2, user0;
+	let ids, idrs;
 
-	before(async () => {
+	beforeEach(async () => {
+
+		await deleteListaCompra();
+		await deleteUsers();
+		await deleteIngredients();
+		await deletePantry();
+		await deleteRecipes();
+		await deleteContains();
+
 		await db.query("INSERT INTO usuarios (username, password) VALUES ('Luis', '123456789')");
 		user = await User.getByUsername("Luis");
 
@@ -26,10 +38,10 @@ describe("Servicio de recetas", () => {
 			[ "harina", "gramos" ],
 			[ "arroz", "gramos" ],
 			[ "leche", "litros" ],
-			[ "harina de avena", "gramos" ]
+			[ "chocolate", "gramos" ]
 		];
 		const ings = await insertIngredients(ingredientes);
-		const ids = ings.map(ing => ing.id);
+		ids = ings.map(ing => ing.id);
 
 		const pantrys = [
 			[ user.id, ids[0], QUINIENTOS ],
@@ -42,11 +54,14 @@ describe("Servicio de recetas", () => {
 
 		const recetas = [
 			[ "receta1", "descripcion1" ],
-			[ "receta2", "descripcion2" ]
+			[ "receta2", "descripcion2" ],
+			[ "receta3", "descripcion3" ],
+			[ "receta4", "descripcion4" ],
+			[ "receta5", "descripcion5" ]
 		];
 		const rects = await insertRecetas(recetas);
 
-		const idrs = rects.map(rec => rec.id);
+		idrs = rects.map(rec => rec.id);
 
 		// Receta, ingrediente, cantidad
 		const contienen = [
@@ -55,13 +70,20 @@ describe("Servicio de recetas", () => {
 			[ idrs[0], ids[2], QUINIENTOS ],
 			[ idrs[0], ids[3], QUINIENTOS ],
 			[ idrs[1], ids[0], QUINIENTOS ],
-			[ idrs[1], ids[1], QUINIENTOS ]
+			[ idrs[1], ids[1], QUINIENTOS ],
+			[ idrs[2], ids[1], SETECIENTOS ],
+			[ idrs[3], ids[0], DOSCIENTOS ],
+			[ idrs[4], ids[0], QUINIENTOS ]
 		];
 		await insertContains(contienen);
+
+		const ingLista = [ [ user.id, ids[1], TRESCIENTOS, "gramos" ] ];
+		await insertListaCompra(ingLista);
 
 	});
 
 	after(async () => {
+		await deleteListaCompra();
 		await deleteUsers();
 		await deleteIngredients();
 		await deletePantry();
@@ -103,7 +125,8 @@ describe("Servicio de recetas", () => {
 		});
 	});
 
-	describe("Obtener los datos de una receta por su id", () => {
+	// eslint-disable-next-line capitalized-comments, spaced-comment
+	/*describe("Obtener los datos de una receta por su id", () => {
 		beforeEach(async () => {
 			await deleteContains();
 			await deleteIngredients();
@@ -220,5 +243,122 @@ describe("Servicio de recetas", () => {
 				assert.strictEqual(error.message, "No hay ingredientes");
 			}
 		});
+	});*/
+
+	describe("Comprobar requisitos de la receta", () => {
+
+		it("Debe de devolver que todos los ingredientes son suficientes ", async () => {
+
+			const result = await Recipe.checkRecipeRequirements(user.id, idrs[1]);
+
+			assert.deepStrictEqual(result, {
+				suficientes: [
+					{ id: ids[0], nombre: "harina", unidades: QUINIENTOS, tipoUnidad: "gramos" },
+					{ id: ids[1], nombre: "arroz", unidades: QUINIENTOS, tipoUnidad: "gramos" }
+				],
+				  	faltantes: []
+			});
+
+		});
+
+		it("Debe de devolver que todos los ingredientes son faltantes ", async () => {
+
+			const result = await Recipe.checkRecipeRequirements(user0.id, idrs[1]);
+
+			assert.deepStrictEqual(result, {
+				suficientes: [],
+				faltantes: [
+					{ id: ids[0], nombre: "harina", unidadesNecesarias: QUINIENTOS, tipoUnidad: "gramos" },
+					{ id: ids[1], nombre: "arroz", unidadesNecesarias: QUINIENTOS, tipoUnidad: "gramos" }
+				]
+			});
+
+		});
+
+		it("Debe de devolver ingredientes que sean suficientes y faltantes ", async () => {
+
+			const result = await Recipe.checkRecipeRequirements(user2.id, idrs[1]);
+
+			assert.deepStrictEqual(result, {
+				suficientes: [ { id: ids[0], nombre: "harina", unidades: QUINIENTOS, tipoUnidad: "gramos" } ],
+				faltantes: [ { id: ids[1], nombre: "arroz", unidadesNecesarias: QUINIENTOS, tipoUnidad: "gramos" } ]
+			});
+
+		});
+
+		it("Debe de calcular bien las unidades necesarias ", async () => {
+
+			const result = await Recipe.checkRecipeRequirements(user.id, idrs[2]);
+
+			assert.deepStrictEqual(result, {
+				suficientes: [],
+				faltantes: [ { id: ids[1], nombre: "arroz", unidadesNecesarias: DOSCIENTOS, tipoUnidad: "gramos" } ]
+			});
+
+		});
 	});
+
+	describe("Cocinar receta", () => {
+
+		it("Debe disminuir correctamente en la despensa los ingredientes de la receta", async () => {
+
+			const requirements = await Recipe.checkRecipeRequirements(user.id, idrs[3]);
+			const result = await Recipe.cookRecipe({
+				userId: user.id,
+				recipeId: idrs[3],
+				suficientes: requirements.suficientes
+			});
+
+			const cantidadPantry = await db.query("SELECT cantidad  FROM despensa WHERE id_usuario = ? AND id_ingrediente = ?", [ user.id, ids[0] ]);
+
+			assert.strictEqual(result.success, true);
+			assert.strictEqual(result.message, "¡Receta cocinada con éxito!");
+			assert.deepStrictEqual(result.usados, requirements.suficientes);
+			assert.deepStrictEqual(cantidadPantry[0].cantidad, TRESCIENTOS);
+
+		});
+
+		it("Debe eliminar los ingredientes cuando se cocina toda su cantidad", async () => {
+
+			const requirements = await Recipe.checkRecipeRequirements(user.id, idrs[4]);
+			const result = await Recipe.cookRecipe({
+				userId: user.id,
+				recipeId: idrs[4],
+				suficientes: requirements.suficientes
+			});
+
+			const cantidadPantry = await db.query("SELECT cantidad  FROM despensa WHERE id_usuario = ? AND id_ingrediente = ?", [ user.id, ids[0] ]);
+
+			assert.strictEqual(result.success, true);
+			assert.strictEqual(result.message, "¡Receta cocinada con éxito!");
+			assert.deepStrictEqual(result.usados, requirements.suficientes);
+			assert.deepStrictEqual( cantidadPantry, []);
+		});
+	});
+
+	describe(" Añadir ingredientes que faltan a la lista de la compra", () => {
+
+		it("Debe de insertar un ingrediente nuevo en la lista", async () => {
+			const requirements = await Recipe.checkRecipeRequirements(user0.id, idrs[2]);
+			const result = await Recipe.addMissingToShoppingList(user0.id, idrs[2], requirements.faltantes);
+			const cantidadLista= await db.query( "SELECT cantidad FROM lista_compra WHERE id_usuario = ? AND id_ingrediente = ?", [ user0.id, ids[1] ]);
+
+			assert.strictEqual(result.success, true);
+			assert.strictEqual(result.message, "Ingredientes añadidos a tu lista de la compra");
+			assert.deepStrictEqual(result.faltantes, requirements.faltantes);
+			assert.deepStrictEqual( cantidadLista[0].cantidad, SETECIENTOS );
+		});
+
+		it("Debe de aumentar la cantidad de un ingrediente de la lista", async () => {
+			const requirements = await Recipe.checkRecipeRequirements(user.id, idrs[2]);
+			const result = await Recipe.addMissingToShoppingList(user.id, idrs[2], requirements.faltantes);
+			const cantidadLista= await db.query( "SELECT cantidad FROM lista_compra WHERE id_usuario = ? AND id_ingrediente = ?", [ user.id, ids[1] ]);
+
+			assert.strictEqual(result.success, true);
+			assert.strictEqual(result.message, "Ingredientes añadidos a tu lista de la compra");
+			assert.deepStrictEqual(result.faltantes, requirements.faltantes);
+			assert.deepStrictEqual( cantidadLista[0].cantidad, QUINIENTOS );
+		});
+	});
+
 });
--- test/testUtils.js
+++ test/testUtils.js
@@ -118,6 +118,16 @@ const insertContains = async contains => {
 	await Promise.all(insertPromises); // Ejecuta todas las inserciones en paralelo
 };
 
+const insertListaCompra = async ingredientes => {
+	const insertPromises = ingredientes.map( ingrediente =>
+		db.query("INSERT INTO lista_compra (id_usuario, id_ingrediente, cantidad, unidad_medida) VALUES (?, ?, ?, ?)", ingrediente));
+	await Promise.all(insertPromises); // Ejecuta todas las inserciones en paralelo
+};
+
+const deleteListaCompra = async () => {
+	await db.query("DELETE FROM lista_compra");
+};
+
 const insertDummy = async () => {
 	await db.query(`INSERT INTO usuarios VALUES (1, "dummy", "dummy")`);
 };
@@ -183,6 +193,8 @@ module.exports = {
 	insertRecetas,
 	deleteContains,
 	insertContains,
+	insertListaCompra,
+	deleteListaCompra,
 	insertDummy,
 	createuser,
 	createTestUsers,
