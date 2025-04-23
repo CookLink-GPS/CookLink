09848bd21be27287628ae86b89ff439181ca7e57
Paula211 <paulalsolla@gmail.com>
2025-04-23 18:35:46 +0200

    vista HU_012 hecha

---
+++ public/js/shoppingListScriptView.js
@@ -0,0 +1,10 @@
+const unidadMap = {
+	"gramos": "g",
+	"kilogramos": "kg",
+	"mililitros": "ml",
+	"litros": "l",
+	"unidades": "ud",
+	"cucharadas": "cda (cucharada)",
+	"cucharaditas": "cdta (cucharadita)",
+	"tazas": "tz (taza)"
+};
